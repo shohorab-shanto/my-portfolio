e2255107b4bcbef632fe3760d4efff9aebc04652
unknown <shohorabshanto@gmail.com>
2022-08-11 17:48:45 +0600

    changes

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,6 +5,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -24,6 +25,18 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="blank" rel='noreferrer' href='https://bd.linkedin.com/in/shohorab-shanto-0aa83521b'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="blank" rel='noreferrer' href='https://github.com/shohorab-shanto'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
     </div>
   )
 }
